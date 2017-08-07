54bb889d5b57ee2ba1f01d0506195dd1c85ac32d
monica lai <monju4177@gmail.com>
2017-08-07 12:37:13 +0800

    answer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def ask
+  end
+
+  def answer
+    @answer = params[:question_type]
+  end
+
 end
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  # get 'questions/answer'
+  # get 'questions/ask'
+  get 'ask', to: 'questions#ask'
+  get 'answer', to: 'questions#answer'
+  root 'questions#ask'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
