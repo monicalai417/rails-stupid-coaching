0f35fb559ef0a0f7d3706f8cea49f1df590344ab
monica lai <monju4177@gmail.com>
2017-08-07 14:27:00 +0800

    answer

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,7 +1,30 @@
 class QuestionsController < ApplicationController
   def answer
+    @question = params[:question_type]
+    @answer = coach_answer(@question)
   end
 
   def ask
   end
+
+  def coach_answer_enhanced(your_message)
+    if your_message == "I'm going back to work right now!".upcase
+      return ""
+    elsif your_message.upcase.include?("?")
+      return "I can feel your motivation! Silly question, get dressed and go to work!"
+    else
+      return "I can feel your motivation! I don't care, get dressed and go to work!"
+    end
+  end
+
+  def coach_answer(your_message)
+    if your_message == "I'm going to work right now!"
+      return ""
+    elsif your_message.include?("?")
+      return "Silly question, get dressed and go to work!"
+    else
+      return "I don't care, get dressed and go to work!"
+    end
+  end
+
 end
